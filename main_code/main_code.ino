1c8ad1cbc0b99fdd1ebdaa5db98de7c4d9ba2f72
Bennett Diamond <bennetthdiamond@gmail.com>
2020-09-18 18:52:29 -0700

    It compiles, still can't read sensors

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -1,7 +1,7 @@
 // DRAGRAKE
-// Written by Danny Maas, Bennett Diamond and Christian BLoemhof
-// Collects pressure transducer data via i2c, processes data to make airspeed readings,
-// Creates WiFi network, hosts a webpage that displays this data
+// Written by Danny Maas, Bennett Diamond and Aria Diamond
+// Collects pressure transducer data to an SD card via SPI
+// Creates WiFi network, hosts a webpage that displays said data
 
 
 // Libraries --------------------------------------------------------------------------------------
@@ -14,31 +14,27 @@
 // Variables ---------------------------------------------------------------------------------------
 
 // Command Registers (SPI commands for the pressure transducer)
-#define Start_Single 0xAA     // Hexadecimal address for single read
-#define Start_Average2 0xAC   // Hexadecimal address for average of 2 reads
-#define Start_Average4 0xAD   // Hexadecimal address for average of 4 reads
-#define Start_Average8 0xAE   // Hexadecimal address for average of 8 reads
-#define Start_Average16 0xAF  // Hexadecimal address for average of 16 reads
+#define Start_Single 0xAA    // Hexadecimal address for single read
+#define Start_Average2 0xAC  // Hexadecimal address for average of 2 reads
+#define Start_Average4 0xAD  // Hexadecimal address for average of 4 reads
+#define Start_Average8 0xAE  // Hexadecimal address for average of 8 reads
+#define Start_Average16 0xAF // Hexadecimal address for average of 16 reads
 
-// Initialing SPI chip select lines
-#define s1Toggle 11           // Pin used to toggle sensor 1
-#define s2Toggle 12           // Pin used to toggle sensor 2
-#define SD_card_CS 10         // Pin used for SD card slave select
-bool sen1 = HIGH;             // Initialize everything high 
-bool sen2 = HIGH;
-
-// Array of all commands to simplify changing modes (SPI stuff)
+// Array of all commands to simplify changing modes
 byte start_comm[5] =
-{ Start_Single,
+  {Start_Single,
   Start_Average2,
   Start_Average4,
   Start_Average8,
-  Start_Average16
-};
-int st_mode = 1;              // Command mode selector
+  Start_Average16};
+int mode = 1;              // Command mode selector
 
-// Array of calibration registers
-int cal_reg[11] = {47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
+// Initialing SPI chip select lines
+#define s1Toggle 11   // Pin used to toggle sensor 1
+#define s2Toggle 12   // Pin used to toggle sensor 2
+#define SD_card_CS 10 // Pin used for SD card slave select
+bool sen1 = HIGH;     // Initialize everything high 
+bool sen2 = HIGH;
 
 // Variables used to measure battery voltage
 #define VBATPIN A7
@@ -46,22 +42,19 @@ float measuredvbat;
 
 // Variables used in calculations
 float coeff[7];
-int32_t rawCoeff[4];
-int8_t rawTempCoeff[3];
-byte lowWord;
-byte highWord;
-byte words[20];
 float sensor1;
 float sensor2;
 float temp1;
 float temp2;
 
 // SD Card Variables
-String current_file = "/data.txt"; // start with default name
+String currentFileString = "/data.txt"; // start with default name
+const char* currentFile = "/data.txt";
 
 // WiFi Variables
 char ssid[] = "DragRake";         // Network Name
 char pass[] = "";                 // Network password
+String XML_values;                // XML data structure
 
 int status = WL_IDLE_STATUS;
 IPAddress local_ip(4,20,6,9);
@@ -72,11 +65,10 @@ IPAddress subnet(255,255,255,0);
 WebServer server(80);
 
 // SPI configurations
-SPISettings sensors(14000000, MSBFIRST, SPI_MODE0); //Both sensors are logic state low
+SPISettings sensors(1000000, MSBFIRST, SPI_MODE0); //Both sensors are logic state low
 
 // Setup -------------------------------------------------------------------------------------------------------
-void setup()
-{
+void setup() {
   // Set one sensor to active, the other to off
   pinMode(s1Toggle, OUTPUT);
   pinMode(s2Toggle, OUTPUT);
@@ -91,14 +83,15 @@ void setup()
   WiFi.softAP(ssid);
   WiFi.softAPConfig(local_ip, gateway, subnet);
 
-  // Setting Webpage modes
-  server.on("/", handle_read(0));
-  server.on("/read1", handle_read(1));
-  server.on("/read2", handle_read(2));
-  server.on("/read4", handle_read(4));
-  server.on("/read8", handle_read(8));
-  server.on("/read16", handle_read(16));
-  server.onNotFound(handle_NotFound);
+  // Initializing webpages
+  server.on("/", handleNoRead);
+  server.on("/read1", handleRead1);
+  server.on("/read2", handleRead2);
+  server.on("/read4", handleRead4);
+  server.on("/read8", handleRead8);
+  server.on("/read16", handleRead16);
+  server.on("/update", handleUpdate);
+  server.onNotFound(handleNotFound);
 
   server.begin();
 
@@ -112,41 +105,41 @@ void setup()
 
   // Creating unique file name to avoid overwriting existing files
   int i; // declare i for while loop
-  File dataFile = SD.open(current_file);
+  File dataFile = SD.open(currentFile);
   
-  while (dataFile // prevents overwriting data by generating a new file if one with the same name already exists
+  while (dataFile) // prevents overwriting data by generating a new file if one with the same name already exists
   {
-    current_file = String("/data" + String(i) + ".txt");
+    currentFileString = String("/data" + String(i) + ".txt");
     i++;
-    dataFile = SD.open(current_file);
+    const char* currentFile = currentFileString.c_str();
+    dataFile = SD.open(currentFile);
   }
 
-  String dataHeader = "Time (from starting)  Top Pressure  Bottom Pressure  Top Temp  Bottom Temp  Battery Voltage \r\n";
-  writeFile(SD, current_file, dataHeader); // write headers to file
+  const char* dataHeader = "Time (from starting)  Top Pressure  Bottom Pressure  Top Temp  Bottom Temp  Battery Voltage \r\n";
+  writeFile(SD, currentFile, dataHeader); // write headers to file
 
   dataFile.close(); // finish file
 
 }
 
 // Loop --------------------------------------------------------------------------------------------------------
-void loop()
-{
+void loop() {
   // Taking and recording data
   Serial.println("I take data now");
   takeMeasurements();
   measuredvbat = analogRead(VBATPIN);
   measuredvbat *= 6.6;  // Multiply by 2 and 3.3V, our reference voltage
   measuredvbat /= 1024; // convert to voltage
-  SD_write(current_file); // Write values to SD Card 
+  SD_write(currentFile); // Write values to SD Card 
   
   // Webpage stuff
   server.handleClient();
+}
 
 // Functions -------------------------------------------------------------------------------------------
 
 // Reads data from both sensors and stores it in global variable
-void takeMeasurements()
-{
+void takeMeasurements() {
   requestRead(1);
   waitForDone(1);
   sensor1 = requestData(1, &temp1);
@@ -157,201 +150,218 @@ void takeMeasurements()
 }
 
 // Get calibration data from sensors (On startup)
-void requestCalib()
-{
-  //Coefficients are the same from both sensors, so we only need them from one
+void requestCalib() {
+  Serial.println("Tryin' to calibrate");
   int j = 0;
-  for (byte i = 0x2F; i < 0x39; i++)
-  {
+  uint16_t words[10];
+  for (byte i = 0x2F; i < 0x39; i++) {
     SPI.beginTransaction(sensors);
     toggle(1);
     SPI.transfer(i);
     SPI.transfer(0x00);
     SPI.transfer(0x00);
-    uint32_t buff = (0xF0) | (0x00 << 8) | 0x00;
-    SPI.transfer(buff);
+    Serial.println("Transfered request");
+    byte outBuff[2];
+    SPI.transfer(0xF0);
+    outBuff[0] = SPI.transfer(0x00);
+    outBuff[1] = SPI.transfer(0x00);
+    Serial.println("Received high and low byte");
 
     // Arduino transmits measurement start command, will receive back status byte and two bytes of "undefined data"
     toggle(1);
     SPI.endTransaction();
 
     // Parse bytes into one "word"
-    word x = word(byte(buff >> 8), byte(buff));
+    uint16_t x = (outBuff[0] << 8) | (outBuff[1]);
 
     // Add piece of data to the words
     words[j++] = x;
-
   }
-  for (int b = 0; b < 4; b++)
-  {
-    rawCoeff[b] = (words[(4 * b)] << 24) | (words[(4 * b) + 1] << 16) | (words[(4 * b) + 2] << 8) | (words[(4 * b) + 3]);
+
+  Serial.println("Exited loop");
+  int32_t rawCoeff[4];
+  for (int b = 0; b < 4; b++) {
+    rawCoeff[b] = (words[(2 * b)] << 16) | (words[(2 * b) + 1]);
     coeff[b] = (float)rawCoeff[b] / (float)0x7FFFFFFF;
   }
 
+  Serial.println("Stacked coefficients");
   // Parse coefficient data to get coefficients
-  rawTempCoeff[0] = words[16];
-  rawTempCoeff[1] = words[17];
-  rawTempCoeff[2] = words[18];
-  coeff[4] = (float)rawTempCoeff[0] / (float)0x7F;
-  coeff[5] = (float)rawTempCoeff[1] / (float)0x7F;
-  coeff[6] = (float)rawTempCoeff[2] / (float)0x7F;
+  int8_t rawTempCoeff[3] = {((byte)words[8] >> 8), (byte)words[8], ((byte)words[9] >> 8)};
+  coeff[4] = (float)rawTempCoeff[0] / 0x7F;
+  coeff[5] = (float)rawTempCoeff[1] / 0x7F;
+  coeff[6] = (float)rawTempCoeff[2] / 0x7F;
+
 }
 
-void toggle(int sensor)
-{
-  if (sensor == 2)
-  {
+void toggle(int sensor) {
+  if (sensor == 2) {
     digitalWrite(s2Toggle, !sen1);
     sen1 = !sen1;
-  }
-  else if (sensor == 1)
-  {
+  } else if (sensor == 1) {
     digitalWrite(s1Toggle, !sen2);
     sen2 = !sen2;
   }
 }
 // Starts read of pressure and temperature data
-int requestRead(int tog)
-{
+int requestRead(int tog) {
   // If button is not set to "Off"
-  if (selected > 0)
+  if (mode > 0)
   {
-    uint8_t buff1 = start_comm[selected - 1] << 16; // WHY IS THIS ONE 16 BITS??
-    uint8_t buff2 = 0x00 << 8; 
+    uint8_t buff1 = start_comm[mode - 1]; // removed bit shift
+    uint8_t buff2 = 0x00; 
     uint8_t buff3 = 0x00;
     SPI.beginTransaction(sensors);
     toggle(tog);
-    SPI.transfer(&buff1); // ESP32 SPI doesn't support multiple byte transfer
-    SPI.transfer(&buff2); // WHY ARE THESE POINTERS?
-    SPI.transfer(&buff3);
+    buff1 = SPI.transfer(buff1); 
+    buff2 = SPI.transfer(buff2); 
+    buff3 = SPI.transfer(buff3);
     //Arduino transmits measurement start command, will receive back status byte and two bytes of "undefined data"
     toggle(tog);
     SPI.endTransaction();
 
-    uint8_t buff = (buff1 | buff2 | buff3); // concatenating bytes
-  }
-  else
-  {
+    uint8_t buff = (buff1 << 16 | buff2 << 8 | buff3); // concatenating bytes
   }
 }
 
 // Allows us to wait for the sensor to be done reading before asking for data back
-int  waitForDone(int tog)
-{
-  int i = 0;
-  bool lineReady = 0;
-  while (lineReady == 0)
-  {
+int  waitForDone(int tog) {
 
+  while (true) {
+    Serial.println("In the while loops");
     // If the sensor is still taking data, the bit will be 1.
     // If it is ready, the bit will be zero, and we can read data.
-    byte Status = 0xF0;
+    const byte Status = 0xF0;
     SPI.beginTransaction(sensors);
     toggle(tog);
 
     //Arduino transmits 0xF0, receives back status byte
-    SPI.transfer(&Status, 3);
+    byte sensorStatus = SPI.transfer(Status);
     toggle(tog);
     SPI.endTransaction();
-    bool lineReady = 0;
-    if (bitRead(Status, 5) == 0)
-    {
-      lineReady = 1;
+
+    Serial.println(sensorStatus & 0x20);
+    if ((sensorStatus & 0x20) == 0) {
       break;
     }
-      else {
-    }
   }
+  Serial.println("Out of while loop");
 }
 
 // Requests temperature and pressure data back from sensors
-float requestData(int tog, float* temperature)
-{
-  int32_t iPraw;
-  float pressure;
-  int32_t iTemp;
-  float AP3, BP2, CP, LCorr, PCorr, Padj, TCadj, TC50, Pnorm;
-  int32_t Tdiff, Tref, iPCorrected;
-  uint32_t uiPCorrected;
-
+float requestData(int tog, float *temperature) {
+  Serial.println("Trying to take sensor data");
   // Get data from SPI lines
+
   SPI.beginTransaction(sensors);
   toggle(tog);
   byte stat = SPI.transfer(0xF0);
-  byte p3 = SPI.transfer(0x00);
+  byte p3 = SPI.transfer(0x00); // (float)
   byte p2 = SPI.transfer(0x00);
   byte p1 = SPI.transfer(0x00);
   byte t3 = SPI.transfer(0x00);
   byte t2 = SPI.transfer(0x00);
   byte t1 = SPI.transfer(0x00);
-  unsigned char data[7] = {stat, p3, p2, p1, t3, t2, t1};
 
   // Arduino transmits read command, gets back lots of data (hopefully)
   toggle(tog);
   SPI.endTransaction();
 
   //Big 'ol calculation (which may not actually be right :/
-  iPraw = ((data[1] << 16) + (data[2] << 8) + (data[3]) - 0x800000);
-  iTemp = (data[4] << 16) + (data[5] << 8) + (data[6]);
-  Pnorm = (float)iPraw;
-  Pnorm /= (float) 0x7FFFFF;
-  AP3 = coeff[0] * Pnorm * Pnorm * Pnorm;
-  BP2 = coeff[1] * Pnorm * Pnorm;
-  CP = coeff[2] * Pnorm;
-  LCorr = AP3 + BP2 + CP + coeff[3];
+  int32_t iPraw = ((p3 << 16) | (p2 << 8) | p1) - 0x800000;
+  int32_t iTemp = (t3 << 16) | (t2 << 8) | t1;
+  float Pnorm = (float)iPraw / 0x7FFFFF;
+
+  float AP3 = coeff[0] * Pnorm * Pnorm * Pnorm;
+  float BP2 = coeff[1] * Pnorm * Pnorm;
+  float CP = coeff[2] * Pnorm;
+  float LCorr = AP3 + BP2 + CP + coeff[3];
 
   // Compute Temperature - Dependent Adjustment:
-  Tref = (int32_t)(pow(2, 24) * 65 / 125); // Reference Temperature, in sensor counts
-  Tdiff = iTemp - Tref;
-  Tdiff = Tdiff / 0x7FFFFF;
+  int32_t Tref = (int32_t)(pow(2, 24) * 65 / 125); // Reference Temperature, in sensor counts
+  int32_t Tdiff = iTemp - Tref;
 
   //TC50: Select High/Low, based on sensor temperature reading:
+  float TC50;
+
   if (iTemp > Tref) {
-    TC50 = (coeff[4] - 1);
+    TC50 = coeff[4];
+  } else {
+    TC50 = coeff[5];
   }
-  else {
-    TC50 = (coeff[5] - 1);
-  }
+
+  float Padj;
   if (Pnorm > 0.5) {
     Padj = Pnorm - 0.5;
-  }
-  else {
+  } else {
     Padj = 0.5 - Pnorm;
   }
 
   // Calculating compensated pressure
-  TCadj = (1.0 - (coeff[6] * 1.25 * Padj)) * Tdiff * TC50;
-  PCorr = Pnorm + LCorr + TCadj; // corrected P: float, ±1.0
-  iPCorrected = (int32_t)(PCorr * (float)0x7FFFFF); // corrected P: signed int32
-  uiPCorrected = (uint32_t) (iPCorrected + 0x800000);
-  pressure = ((float)12.5 * ((float)uiPCorrected / (float)pow(2, 23)));
-  pressure = pressure - (float) 9.69;
+  float TCadj = (1.0 - (coeff[6] * 1.25 * Padj)) * Tdiff * TC50;
+  float PCorr = Pnorm + LCorr + TCadj;                    // corrected P: float, ±1.0
+  int32_t iPCorrected = (int32_t)(PCorr * (float)0x7FFFFF); // corrected P: signed int32
+  uint32_t uiPCorrected = (uint32_t)(iPCorrected + 0x800000);
+
+  float pressure = (float)1.25 * ((float)(uiPCorrected - (0.5*pow(2,24)))/ (float)pow(2, 24)) * 2490.889; // convert to Pa
 
   // Calculating temperature (degrees Celcius)
-  *temperature = ((iTemp * 125)/pow(2,24)) - 40; // Celcius
+  *temperature = ((iTemp * 125) / pow(2, 24)) - 40; // Celcius
 
   return pressure;
 }
 
-void handle_read(int mode) 
-{
-  if (mode == 0)
-  {
-    Serial.println("Not readings sensors");
-  } 
-  else if (mode == 1)
-  {
-    Serial.println("Single sensor read");     
-  } 
-  else 
-  {
-    Serial.println("Average of " + String(mode) + " readings")
-  }
+void handleNoRead() {
+  Serial.println("Not reading sensors");
   server.send(200, "text/html", SendHTML(mode)); 
 }
 
-void handle_NotFound(){
+void handleRead1() {
+  Serial.println("Single sensor read");   
+  server.send(200, "text/html", SendHTML(mode));
+}
+
+void handleRead2() {
+  Serial.println("Average of 2 readings");
+  server.send(200, "text/html", SendHTML(mode)); 
+}
+
+void handleRead4() {
+  Serial.println("Average of 2 readings");
+  server.send(200, "text/html", SendHTML(mode)); 
+}
+
+void handleRead8() {
+  Serial.println("Average of 2 readings");
+  server.send(200, "text/html", SendHTML(mode)); 
+}
+
+void handleRead16() {
+  Serial.println("Average of 16 readings");
+  server.send(200, "text/html", SendHTML(mode)); 
+}
+
+void handleNotFound() {
   server.send(404, "text/plain", "Not found");
+}
+
+void handleUpdate() {
+  // Creating XML values to send data to webpage
+  XML_values  ="<inputs><upper>";
+  XML_values += String(sensor1);
+  XML_values +="</upper>";
+  XML_values +="<lower>";
+  XML_values += String(sensor2);
+  XML_values +="</lower>";
+  XML_values +="<average>";
+  XML_values += String((sensor1 + sensor2) / (float)2);
+  XML_values +="</average>";
+  XML_values +="<battery>";
+  XML_values +=String(measuredvbat);
+  XML_values +="</battery>";
+  XML_values +="</inputs>";
+
+  server.send(200, "text/plain", XML_values);
 }
 
 String SendHTML(int mode)
@@ -381,43 +391,28 @@ String SendHTML(int mode)
   ptr +=".low {border-color: #000000;color: black;}";
   ptr +=".avg {border-color: #000000;color: black;}";
   ptr +=".batt {border-color: #000000;color: black;}";  
-  
-  // Data Input
-  ptr +="<inputs><upper>";
-  ptr += String(sensor1);
-  ptr +="</upper>";
-  ptr +="<lower>";
-  ptr += String(sensor2);
-  ptr +="</lower>";
-  ptr +="<average>";
-  ptr += String((sensor1 + sensor2) / (float)2);
-  ptr +="</average>";
-  ptr +="<battery>";
-  ptr +=String(measuredvbat);
-  ptr +="</battery>";
-  ptr +="</inputs>";
 
   ptr +="</style>\n";
   ptr +="</head>\n";
   ptr +="<body>\n";
 
   // Title
+  ptr +="<div>";
   ptr +="<h1>Drag Rake</h1>\n";
   ptr +="<h3>Akaflieg SLO ESP32 Proto-build</h3>\n";
   ptr +="<!-- EASTER EGG -->";
 
   // Data displays
-  ptr +=("<h2><input class=\"read upp\" id=\"Upper\" value=\"0\"/>Upper Surface &Delta;Pressure (Pa)</h2>";
-  ptr +="<h2><input class=\"read low\" id=\"Lower\" value="
-  ptr +=String(sensor2); // atempting to see if this works
-  ptr +="/>Lower Surface &Delta;Pressure (Pa)</h2>";
+  ptr +="<h2><input class=\"read upp\" id=\"Upper\" value=\"0\"/>Upper Surface &Delta;Pressure (Pa)</h2>";
+  ptr +="<h2><input class=\"read low\" id=\"Lower\" value=\"0\"/>Lower Surface &Delta;Pressure (Pa)</h2>";
   ptr +="<h2><input class=\"read avg\"id=\"Average\" value=\"0\"/>Average &Delta;Pressure (Pa)</h2>";
   ptr +="<h2><input class=\"read batt\"id=\"Battery\" value=\"0\"/>Battery Remaining (%)</h2>";
+  ptr +="</div>";
 
   // Buttons
+  ptr +="<div>";
   ptr +="<h3>Sensor Read Modes</h3>\n";
-  switch (mode)
-  {
+  switch (mode) {
     case 0:
     ptr +="<a class=\"button button-off\" href=\"/\">OFF</a>";
     ptr +="<a class=\"button button-on\" href=\"/read1\">ON</a>";
@@ -472,20 +467,41 @@ String SendHTML(int mode)
     ptr +="<a class=\"button button-off\" href=\"/read16\">OFF</a>\n";
     break;
   }
+  ptr +="</div>";
+
+  // AJAX Script (move to top of document if possible just for organization)
+  ptr = "<script>";
+  ptr = "setInterval(function() {"; // Call a function repetatively with 2 Second interval
+  ptr = "getData();}, 200);"; // 200 millis update rate
+  ptr = "function getData() {";
+  ptr = "var xhttp = new XMLHttpRequest();";
+  ptr = "xhttp.onreadystatechange = function() {";
+  ptr = "  if (this.readyState == 4 && this.status == 200) {";
+  ptr = "   document.getElementById(\"Upper\").value =this.responseXML.getElementsByTagName('upper')[0].childNodes[0].nodeValue;";
+  ptr = "   document.getElementById(\"Lower\").value =this.responseXML.getElementsByTagName('lower')[0].childNodes[0].nodeValue;";
+  ptr = "   document.getElementById(\"Average\").value =this.responseXML.getElementsByTagName('average')[0].childNodes[0].nodeValue;";
+  ptr = "   document.getElementById(\"Battery\").value =this.responseXML.getElementsByTagName('battery')[0].childNodes[0].nodeValue;";
+  ptr = "  }";
+  ptr = "};";
+  ptr = "xhttp.open(""GET"", ""update"", true);";
+  ptr = "xhttp.send();";
+  ptr = "}";
+  ptr = "</script>";
 
   ptr +="</body>\n";
   ptr +="</html>\n";
   return ptr; 
 }
 
-void SD_write(String current_file)
+void SD_write(const char* currentFile)
 {
   // Creating Data String, see top for formatting
   String dataString = String(millis()) + "  " + String(sensor1) + "  " + String(sensor2) + 
   "  " + String(temp1) + "  " + String(temp2) + "  " + String(measuredvbat) + "\r\n";
+  const char* data = dataString.c_str();
 
   // open and apend data to the file
-  appendFile(SD, current_file, dataString.c_str());
+  appendFile(SD, currentFile, data);
 }
 
 // Weird ESP32 SD card functions (DON'T TOUCH THESE) -----------------------------------------------
